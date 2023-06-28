f217930793fa1fd356dff39881a4a02fbd7f4add
keepBlank <front-endligen@foxmail.com>
2023-06-28 19:33:38 +0800

    Vue语法练习

---
+++ vue-cli-demo/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <input type="text" v-model="inputVal">
+    <button @click="handleAddItem" class="button">提交</button>
+    <ul>
+      <list-item 
+      v-for="item in list"
+       :key="index"
+       :msg="item"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue';
+import ListItem from './components/ListItem.vue'
+
+export default {
+  name: 'App',
+  components:{
+    ListItem
+  },
+  setup() {
+    const inputVal = ref('')
+    const list = reactive([])
+    const handleAddItem = () => {
+      list.push(inputVal.value)
+      inputVal.value = ''
+    }
+    return { list, handleAddItem, inputVal }
+  }
+}
+</script>
+
+<style>
+  .button{
+    margin-left: 20px;
+  }
+
+</style>
